d0a36fbca6681c0c7d1b9fc69ff0853819b6a7ad
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-05 14:53:42 -0500

    Initial Commit

---
+++ NYT-Books-GroupProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYT-Books-GroupProj
+//
+//  Created by Christian Hurtado on 2/5/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
