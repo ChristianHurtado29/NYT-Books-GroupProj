5339108bb222a10bef94cfbc1932aac1b2480647
Eddie O'Neill <ejosephoneill@gmail.com>
2020-02-18 12:05:06 -0500

    updated BooksCVC and bestcellerController

---
+++ NYT-Books-GroupProj/Secrets/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  NYT-Books-GroupProj
+//
+//  Created by Edward O'Neill on 2/18/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let appKey = "WwhHX5RTj3XV3X6HuGlXHiWMDkU2aEz8"
+}
