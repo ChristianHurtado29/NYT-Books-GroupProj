22947f6e782ca04c41d42f7a98c8072e514dacba
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-18 10:41:10 -0500

    created genres models

---
+++ NYT-Books-GroupProj/Model/GenresModel.swift
@@ -0,0 +1,22 @@
+//
+//  GenresModel.swift
+//  NYT-Books-GroupProj
+//
+//  Created by Christian Hurtado on 2/18/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import Foundation
+
+struct GenresResults: Codable & Equatable {
+    let results: Genres
+}
+
+struct Genres: Codable & Equatable {
+    let listName: String
+    let displayName: String
+    private enum CodingKeys: String, CodingKey{
+        case listName = "list_name"
+        case displayName = "display_name"
+    }
+}
